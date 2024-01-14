426d2b6838e1a56d2a9edef26607567b0e96de2e
da-yoshi-k <hey234234s@gmail.com>
2024-01-14 19:51:52 +0900

    fix: 不要なテストの削除

--- spec/requests/user_sessions_spec.rb
+++ spec/requests/user_sessions_spec.rb
@@ -2,23 +2,6 @@ require 'rails_helper'
 
 RSpec.describe 'UserSessions', type: :request do
   describe 'GET /new' do
-    it 'returns http success' do
-      get '/user_sessions/new'
-      expect(response).to have_http_status(:success)
-    end
-  end
-
-  describe 'GET /create' do
-    it 'returns http success' do
-      get '/user_sessions/create'
-      expect(response).to have_http_status(:success)
-    end
-  end
-
-  describe 'GET /destroy' do
-    it 'returns http success' do
-      get '/user_sessions/destroy'
-      expect(response).to have_http_status(:success)
-    end
+    pending "add some examples (or delete) #{__FILE__}"
   end
 end

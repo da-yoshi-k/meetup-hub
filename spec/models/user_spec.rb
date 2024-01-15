402d33d85709b614ad34c0833c855c2373193f8e
da-yoshi-k <hey234234s@gmail.com>
2024-01-15 21:02:21 +0900

    add: userのmodel Specを追加

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'バリデーション' do
+    it 'email, password, password_confirmationがあれば有効な状態であること' do
+      user = User.new(
+        email: 'test@example.com',
+        password: 'password',
+        password_confirmation: 'password'
+      )
+      expect(user).to be_valid
+    end
+  end
 end

620516fb25662e9b7acb390825402253410977af
da-yoshi-k <hey234234s@gmail.com>
2024-01-18 22:22:45 +0900

    fix: ログイン失敗時の挙動を追加

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -1,14 +1,15 @@
 class UserSessionsController < ApplicationController
   def new
+    @user = User.new
   end
 
   def create
     @user = login(params[:email], params[:password])
-
     if @user
       redirect_back_or_to root_path
     else
-      render action: 'new'
+      @errors = ['メールアドレスまたはパスワードが間違っています。']
+      render :new, status: :unprocessable_entity
     end
   end
 

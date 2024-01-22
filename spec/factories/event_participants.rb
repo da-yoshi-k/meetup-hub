8a1b8d5090f6d571b47b70e27969d7e3061f2159
da-yoshi-k <hey234234s@gmail.com>
2024-01-23 00:22:07 +0900

    test: associationを追加

--- spec/factories/event_participants.rb
+++ spec/factories/event_participants.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :event_participant do
-    user { nil }
-    event { nil }
+    association :user
+    association :event
     location { Faker::Address.city }
   end
 end
